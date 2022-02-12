7ce6d605a91c86daf2467b20f1c3a7e07d24e1fc
lucas vinicius <lucasviniciusaalves@gmail.com>
2022-02-12 10:00:17 -0300

    add: Create Task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,6 +16,14 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle !== '') {
+      const task = {
+        id: Math.floor((Math.random() * 100) + 1),
+        title: newTaskTitle,
+        isComplete: false
+      }
+      setTasks([...tasks, task])
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
